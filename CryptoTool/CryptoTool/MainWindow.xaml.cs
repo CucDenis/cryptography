6874c3834f938b499dda7d56730c78dabfa45297
Cuc Denis <48867203+CucDenis@users.noreply.github.com>
2020-12-08 12:35:11 +0200

    Add files via upload

---
+++ CryptoTool/CryptoTool/MainWindow.xaml.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CryptoTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+       static string sFilleNames = "";
+       static string plainText = "";
+       byte[] cipherText;
+       byte[] cipherKey;
+       byte[] cipherIV;
+       static string algorithmName = "";
+       static string selectedRb = "";
+       static List<string> encryptedText = new List<string>();
+       static List<string> decryptedText = new List<string>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.Filter = "Log Files|*.log|Textfiles|*.txt|All Files|*.*";
+            fileDialog.DefaultExt = ".txt";
+            Nullable<bool> dialogOk = fileDialog.ShowDialog();
+
+            sFilleNames = "";
+
+            if (dialogOk == true)
+            {
+                foreach (string sFileName in fileDialog.FileNames)
+                {
+                    sFilleNames += ";" + sFileName;
+                }
+                sFilleNames = sFilleNames.Substring(1);
+
+                txbFileName.Text = sFilleNames;
+            }
+        }
+
+        private void RunEncrypDecryp(object sender, RoutedEventArgs e)
+        {
+            switch (algorithmName)
+            {
+                case "Rijndael":
+                    RijndaelEncDec();
+                    break;
+                case "AES":
+                    AESEncDec();
+                    break;
+                case "RC2":
+                    RC2EncDec();
+                    break;
+                case "TripleDES":
+                    TripleDESEncDec();
+                    break;
+                case "DES":
+                    RijndaelEncDec();
+                    break;
+                default:
+                    RijndaelEncDec();
+                    break;
+            }
+        }
+
+        void PrintText(object sender, SelectionChangedEventArgs args)
+        {
+            ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
+            algorithmName = lbi.Content.ToString();
+        }
+
+        private void RijndaelEncDec()
+        {
+            try
+            {
+
+                using (Rijndael myRijndael = Rijndael.Create())
+                {
+                    if (selectedRb == "Encryption")
+                    {
+                        encryptedText.Clear();
+                        PlainTextRead();
+                        // Encrypt the string to an array of bytes.
+                        byte[] encrypted = RijndaelCrypto.EncryptStringToBytes(plainText, cipherKey = myRijndael.Key, cipherIV = myRijndael.IV);
+                        cipherText = encrypted;
+                        encryptedText.Add(Convert.ToBase64String(encrypted));
+                        txbEncryDecryp.Text = encryptedText[0];
+                    }
+                    else if (selectedRb == "Decryption")
+                    {
+                        string roundtrip = RijndaelCrypto.DecryptStringFromBytes(cipherText, cipherKey, cipherIV);
+                        txbEncryDecryp.Text = roundtrip;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+
+        private void AESEncDec()
+        {
+            try
+            {
+
+                using (Aes myAes = Aes.Create())
+                {
+                    if (selectedRb == "Encryption")
+                    {
+                        encryptedText.Clear();
+                        PlainTextRead();
+                        // Encrypt the string to an array of bytes.
+                        byte[] encrypted = AES.EncryptStringToBytes_Aes(plainText, cipherKey = myAes.Key, cipherIV = myAes.IV);
+                        cipherText = encrypted;
+                        encryptedText.Add(Convert.ToBase64String(encrypted));
+                        txbEncryDecryp.Text = encryptedText[0];
+                    }
+                    else if (selectedRb == "Decryption")
+                    {
+                        string roundtrip = AES.DecryptStringFromBytes_Aes(cipherText, cipherKey, cipherIV);
+                        txbEncryDecryp.Text = roundtrip;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+
+        private void TripleDESEncDec()
+        {
+            try
+            {
+                encryptedText.Clear();
+                PlainTextRead();
+                // Create a new TripleDESCryptoServiceProvider object
+                // to generate a key and initialization vector (IV).
+                TripleDESCryptoServiceProvider tDESalg = new TripleDESCryptoServiceProvider();
+                if (selectedRb == "Encryption")
+                {
+                    // Encrypt the string to an in-memory buffer.
+                    byte[] Data = TripleDES.EncryptTextToMemory(plainText, cipherKey = tDESalg.Key, cipherIV = tDESalg.IV);
+                    cipherText = Data;
+                    encryptedText.Add(Convert.ToBase64String(Data));
+                    txbEncryDecryp.Text = encryptedText[0];
+                }
+                else if (selectedRb == "Decryption")
+                {
+
+                    // Decrypt the buffer back to a string.
+                    string roundTrip = TripleDES.DecryptTextFromMemory(cipherText, cipherKey, cipherIV);
+                    txbEncryDecryp.Text = roundTrip;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void RC2EncDec()
+        {
+            try
+            {
+
+                using (RC2CryptoServiceProvider rc2CSP = new RC2CryptoServiceProvider())
+                {
+                    if (selectedRb == "Encryption")
+                    {
+                        encryptedText.Clear();
+                        PlainTextRead();
+                        // Encrypt the string to an array of bytes.
+                        byte[] encrypted = RC2.EncryptStringToBytes(plainText, cipherKey = rc2CSP.Key, cipherIV = rc2CSP.IV);
+                        cipherText = encrypted;
+                        encryptedText.Add(Convert.ToBase64String(encrypted));
+                        txbEncryDecryp.Text = encryptedText[0];
+                    }
+                    else if (selectedRb == "Decryption")
+                    {
+                        string roundtrip = RC2.DecryptStringFromBytes(cipherText, cipherKey, cipherIV);
+                        txbEncryDecryp.Text = roundtrip;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+
+        private void DESEncDec()
+        {
+            try
+            {
+                encryptedText.Clear();
+                PlainTextRead();
+                // Create a new TripleDESCryptoServiceProvider object
+                // to generate a key and initialization vector (IV).
+                DESCryptoServiceProvider DESalg = new DESCryptoServiceProvider();
+                if (selectedRb == "Encryption")
+                {
+                    // Encrypt the string to an in-memory buffer.
+                    byte[] Data = TripleDES.EncryptTextToMemory(plainText, cipherKey = DESalg.Key, cipherIV = DESalg.IV);
+                    cipherText = Data;
+                    encryptedText.Add(Convert.ToBase64String(Data));
+                    txbEncryDecryp.Text = encryptedText[0];
+                }
+                else if (selectedRb == "Decryption")
+                {
+
+                    // Decrypt the buffer back to a string.
+                    string roundTrip = TripleDES.DecryptTextFromMemory(cipherText, cipherKey, cipherIV);
+                    txbEncryDecryp.Text = roundTrip;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void PlainTextRead()
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(sFilleNames))
+                {
+                    plainText = sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void decryption_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if ((bool)rb.IsChecked)
+            {
+                // Keep track of the selected RadioButton by saving a reference
+                // to it.
+                selectedRb = "Decryption";
+            }
+        }
+
+        private void encryption_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if ((bool)rb.IsChecked)
+            {
+                // Keep track of the selected RadioButton by saving a reference
+                // to it.
+                selectedRb = "Encryption";
+            }
+
+        }
+    }
+}
---
+++ CryptoTool/CryptoTool/RC2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CryptoTool
+{
+    class RC2
+    {
+        public static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText missing");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key missing");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV missing");
+            byte[] encrypted;
+            // Create an Rijndael object
+            // with the specified key and IV.
+            using (RC2CryptoServiceProvider rc2CSP = new RC2CryptoServiceProvider())
+            {
+                rc2CSP.Key = Key;
+                rc2CSP.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = rc2CSP.CreateEncryptor(rc2CSP.Key, rc2CSP.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        public static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Rijndael object
+            // with the specified key and IV.
+            using (RC2CryptoServiceProvider rc2CSP = new RC2CryptoServiceProvider())
+            {
+                rc2CSP.Key = Key;
+                rc2CSP.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = rc2CSP.CreateDecryptor(rc2CSP.Key, rc2CSP.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+    }
+}
